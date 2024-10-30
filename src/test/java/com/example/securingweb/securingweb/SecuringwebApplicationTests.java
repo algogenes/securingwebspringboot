34562a1b86a7e5693d2f188b929e6b2004a2a354
algogenes <nyenjesimonpeter@gmail.com>
2024-10-30 18:33:26 +0300

    first commit

---
+++ src/test/java/com/example/securingweb/securingweb/SecuringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingweb.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
